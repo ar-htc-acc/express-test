1c2bd74b72b3525fc86593d4469697ef31c38d01
Louis_Huang <ar.htc.acc@gmail.com>
2016-12-23 17:43:37 +0800

    Temporary...
    Signed-off-by: Louis_Huang <ar.htc.acc@gmail.com>

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-// var users = require('./routes/users');
+var users = require('./routes/users');
 var notes = require('./routes/notes');
 
 var app = express();
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-// app.use('/users', users);
+app.use('/users', users.router);
 app.use('/notes', notes);
 
 // static files:
